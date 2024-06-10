41c4724de80fa68b4c3d4263743e08dc194cf57e
hphats <phatlee1104@gmail.com>
2024-06-10 19:17:27 +0700

    add fzf-git and add some config for zshrc

---
+++ src/shell/aliases.zsh
@@ -0,0 +1,7 @@
+alias fzf="fzf --height 55% --border"
+alias ls="eza --icons=always"
+alias perm="eza --icons=always --long --no-user --no-filesize --no-time"
+alias la="eza --icons=always --long --all"
+alias l="eza --icons=always --long"
+alias tree="eza --icons=always --tree --color=always --level=4 --group-directories-first"
+alias cd="z"
---
+++ src/shell/bindkey.zsh
@@ -0,0 +1,4 @@
+bindkey '[C' forward-word
+bindkey '[D' backward-word
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
