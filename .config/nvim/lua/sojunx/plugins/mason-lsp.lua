b607ead44e91d1be4a33823dd08f5ad782d73bfc
sojunx <phatlee1104@gmail.com>
2025-11-26 19:21:35 +0700

    update nvim

--- .config/nvim/lua/sojunx/plugins/mason-lsp.lua
+++ .config/nvim/lua/sojunx/plugins/mason-lsp.lua
@@ -1,18 +1,44 @@
 return {
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { { "mason-org/mason.nvim", opts = {} }, "neovim/nvim-lspconfig" },
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-			},
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		opts = {
-			ensure_installed = { "stylua" },
-		},
-	},
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { { "mason-org/mason.nvim", opts = {} }, "neovim/nvim-lspconfig" },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "vtsls",
+        "tailwindcss",
+        "html",
+        "cssls",
+        "gopls",
+        "jdtls",
+        "pyright",
+        "jsonls",
+        "yamlls",
+        "marksman",
+        "bashls",
+        "dockerls",
+        "docker_compose_language_service",
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "prettier",
+        "eslint",
+        "stylelint",
+        "golangci-lint",
+        "goimports",
+        "google-java-format",
+        "checkstyle",
+        "ruff",
+        "yamllint",
+        "jsonlint",
+        "shellcheck",
+      },
+    },
+  },
 }
