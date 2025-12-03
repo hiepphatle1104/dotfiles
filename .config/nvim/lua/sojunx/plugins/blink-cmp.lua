3615b43125034944779cb72f6db31889145d957b
sojunx <phatlee1104@gmail.com>
2025-12-03 10:37:33 +0700

    update

--- .config/nvim/lua/sojunx/plugins/blink-cmp.lua
+++ .config/nvim/lua/sojunx/plugins/blink-cmp.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
   dependencies = {
     "rafamadriz/friendly-snippets",
-    "nvim-web-devicons",
+    "nvim-tree/nvim-web-devicons",
     "onsails/lspkind.nvim",
     {
       "saghen/blink.compat",
